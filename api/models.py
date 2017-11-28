709368c0955fe08f27668cc87c256e482e488d0c
younggeeks <younggeeks101@gmail.com>
2017-11-28 11:40:40 +0300

    [CHORE] update comments for models.py

--- api/models.py
+++ api/models.py
@@ -1,6 +1,16 @@
 
 class User:
+    """
+    Model class for User
+    """
     def __init__(self, id, full_name, email, password):
+        """
+        constructor which will be called every time User object is instantiated
+        :param id:
+        :param full_name:
+        :param email:
+        :param password:
+        """
         self.id = id
         self.full_name = full_name
         self.email = email
@@ -8,7 +18,21 @@ class User:
 
 
 class Event:
+    """
+    Model class for Event
+    """
     def __init__(self, id,  name, address, start_date, end_date, user, description, category):
+        """
+        constructor which will be called every time User object is instantiated
+        :param id:
+        :param name:
+        :param address:
+        :param start_date:
+        :param end_date:
+        :param user:
+        :param description:
+        :param category:
+        """
         self.name = name
         self.id = id
         self.address = address
