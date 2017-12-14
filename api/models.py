937121d035ae607f3af55217be2ba5757b24e504
younggeeks <younggeeks101@gmail.com>
2017-12-14 22:13:48 +0300

    [Fix #152997584] embed delete function to Event Model

--- api/models.py
+++ api/models.py
@@ -100,4 +100,8 @@ class Event(db.Model):
         db.session.add(self)
         db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
     __tablename__ = "events"
