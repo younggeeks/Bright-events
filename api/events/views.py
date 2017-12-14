100237739cf272a80788ee9aae1fd19193f1aec1
younggeeks <younggeeks101@gmail.com>
2017-12-14 06:53:28 +0300

    [Feature #152997435] add post event functionality

---
+++ api/events/views.py
@@ -0,0 +1,77 @@
+from functools import wraps
+
+from flask import Blueprint, request, jsonify, g, abort
+from flask_restplus import Api, Resource
+
+events = Blueprint("events", __name__, url_prefix="/api/v1/events")
+
+api = Api(events, catch_all_404s=True)
+
+from api.models import Event, User
+
+
+def protected_route(f):
+    @wraps(f)
+    def func_wrapper(*args, **kwargs):
+        bearer_token = request.headers.get("Authorization")
+        if not bearer_token:
+            response = jsonify({
+                "message": "Token is missing"
+            })
+            response.status_code = 401
+            return response
+
+        token = bearer_token.replace("Bearer ", "")
+        resp = User.decode_token(token)
+        if isinstance(resp, int):
+            user = User.query.filter_by(id=resp).first()
+            if not user:
+                response = jsonify({
+                    "message": "User not found "
+                })
+                response.status_code = 404
+                return response
+            g.user = user
+            return f(*args, **kwargs)
+        else:
+            response = jsonify({
+                "message": resp
+            })
+            response.status_code = 401
+            return response
+
+    return func_wrapper
+
+
+@api.route("")
+class EventList(Resource):
+    def get(self):
+        pass
+
+    @protected_route
+    def post(self):
+        data = request.get_json()
+
+        if "name" in data and "address" in data and "start_date" in data and "end_date" in data and "description" \
+                in data and "price" in data and "category_id" in data:
+            event = Event.query.filter_by(name=data["name"]).first()
+            if event:
+                response = jsonify({"message": "Event name must be unique"})
+                response.status_code = 400
+                return response
+            else:
+                new_event = Event(name=data["name"], address=data["address"], start_date=data["start_date"],
+                                  end_date=data["end_date"], description=data["description"], price=data["price"],
+                                  category_id=data["category_id"])
+                user = g.user
+                user.events.append(new_event)
+                user.save()
+                response = jsonify({"message": "Event Registration Successfully"})
+                response.status_code = 201
+                return response
+        else:
+            response = jsonify({
+                "message": "Event Registration failed, Please check your input"
+            })
+            response.status_code = 400
+            return response
