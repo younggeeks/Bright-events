a83aa3ff9ee5b47a324736ec98ebd902c625fda9
younggeeks <younggeeks101@gmail.com>
2017-12-14 06:49:42 +0300

    [Feature] register events blueprint

--- run.py
+++ run.py
@@ -2,12 +2,14 @@ import os
 
 from api import create_app
 from api.auth.views import auth
+from api.events.views import events
 
 environment = os.getenv("ENV_SETTINGS")
 app = create_app(environment)
 
 
 app.register_blueprint(auth)
+app.register_blueprint(events)
 
 if __name__ == '__main__':
     app.run()
