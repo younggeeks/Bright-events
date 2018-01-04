921aeffcdc5be052f7c90d1bacd31fb77ae3a9d8
Samjunior <younggeeks101@gmail.com>
2018-01-04 23:24:46 +0300

    [Fix] move  blueprint registration to __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,12 +5,18 @@ from instance.config import env_config
 
 db = SQLAlchemy()
 
+from api.auth.views import auth
+from api.events.views import events
+
 
 def create_app(environment):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(env_config[environment])
     app.config.from_pyfile("config.py")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.register_blueprint(auth)
+    app.register_blueprint(events)
     db.init_app(app)
-    print(os.getenv("POSTGRES_DATABASE_URI"))
+
     return app
