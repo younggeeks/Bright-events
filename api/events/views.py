667a9c187f9e4cbd4fb40338761089540f8e54a1
Samjunior <younggeeks101@gmail.com>
2018-01-04 23:27:37 +0300

    [Fix] fix events endpoints to improve coverage

--- api/events/views.py
+++ api/events/views.py
@@ -1,91 +1,50 @@
 from functools import wraps
 
-from flask import Blueprint, request, jsonify, g, abort, url_for
-from flask_restplus import Api, Resource
+from flask import Blueprint, request, jsonify, g, url_for
+from flask_restful import Api, Resource
 from sqlalchemy import asc
+
+from api.helpers.response_helpers import protected_route
 
 events = Blueprint("events", __name__, url_prefix="/api/v1/events")
 
-api = Api(events, version='1.0', title='Bright Events API',
-          description='Awesome Api for managing events', catch_all_404s=True
-          )
+api = Api(events, catch_all_404s=True)
 
 from api.models import Event, User
-from api import response_helpers
+from api.helpers import response_helpers
 
 
 def search(search_type):
-    parser = api.parser()
-    parser.add_argument('q', type=str, help='Event Name', location='args')
-    args = parser.parse_args()
-    if not args['q']:
+    q = request.args.get("q")
+    if not q or q == "":
         response = jsonify({
             "message": "Query Not Specified, Search Failed"
         })
         response.status_code = 400
         return response
     if search_type == "name":
-        found_events = Event.query.filter(Event.name.ilike('%{}%'.format(args['q']))).all()
+        found_events = Event.query.filter(Event.name.ilike('%{}%'.format(q))).all()
     elif search_type == "location":
-        found_events = Event.query.filter(Event.address.ilike('%{}%'.format(args['q']))).all()
-    elif search_type == "category":
-        found_events = Event.query.filter(Event.category.name.ilike('%{}%'.format(args['q']))).all()
-    else:
-        found_events = Event.query.filter(Event.name.ilike('%{}%'.format(args['q']))).all()
+        found_events = Event.query.filter(Event.address.ilike('%{}%'.format(q))).all()
 
     if found_events and len(found_events) > 0:
         response = jsonify({
-            "message": "Successfully Retrieved Events Matching {}".format(args['q']),
-            "guests": response_helpers.parse_list("events", found_events)
+            "message": "Successfully Retrieved Events Matching {}".format(q),
+            "events": response_helpers.parse_list("events", found_events)
         })
         response.status_code = 200
         return response
     else:
         response = jsonify({
-            "message": "Event Matching {} Was not found".format(args['q'])
+            "message": "Event Matching {} Was not found".format(q)
         })
         response.status_code = 404
         return response
 
 
-def protected_route(f):
-    @wraps(f)
-    def func_wrapper(*args, **kwargs):
-        bearer_token = request.headers.get("Authorization")
-        if not bearer_token:
-            response = jsonify({
-                "message": "Token is missing"
-            })
-            response.status_code = 401
-            return response
-
-        token = bearer_token.replace("Bearer ", "")
-        resp = User.decode_token(token)
-        if isinstance(resp, int):
-            user = User.query.filter_by(id=resp).first()
-            if not user:
-                response = jsonify({
-                    "message": "User not found "
-                })
-                response.status_code = 404
-                return response
-            g.user = user
-            return f(*args, **kwargs)
-        else:
-            response = jsonify({
-                "message": resp
-            })
-            response.status_code = 401
-            return response
-
-    return func_wrapper
-
-
-@api.route("")
 class EventList(Resource):
     def get(self):
         all_events = Event.query.all()
-        print(response_helpers.event_parser(all_events[0]))
         response = jsonify({
             "message": "Events Retrieved Successfully",
             "events": response_helpers.parse_list("events", all_events)
@@ -122,23 +81,23 @@ class EventList(Resource):
             return response
 
 
-@api.route("/<int:event_id>")
 class Events(Resource):
     @protected_route
     def put(self, event_id):
         event = Event.query.filter_by(id=event_id).first()
-        if event.user_id != g.user.id:
-            response = jsonify({
-                "message": "You can only Update Events You Created"
-            })
-            response.status_code = 403
-            return response
         if not event:
             response = jsonify({
                 "message": "Event With ID {} is not found".format(event_id)
             })
             response.status_code = 404
             return response
+        if event.user_id != g.user.id:
+            response = jsonify({
+                "message": "You can only Update Events You Created"
+            })
+            response.status_code = 403
+            return response
+
         data = request.get_json()
         if not data:
             response = jsonify({
@@ -201,13 +160,12 @@ class Events(Resource):
 
         event.delete()
         response = jsonify({
-            "message": "Event Deletion Successfully"
+            "message": "Event Deletion Successful"
         })
         response.status_code = 200
         return response
 
 
-@api.route("/<int:event_id>/rsvp")
 class RSVP(Resource):
     @protected_route
     def post(self, event_id):
@@ -232,7 +190,7 @@ class RSVP(Resource):
             response = jsonify({
                 "message": "You can not RSVP To your own event"
             })
-            response.status_code = 404
+            response.status_code = 403
             return response
 
         existing_user = [user for user in event.rsvps if user.id == guest.id]
@@ -240,7 +198,7 @@ class RSVP(Resource):
             response = jsonify({
                 "message": "Your Name is already in {}'s Guest List".format(event.name)
             })
-            response.status_code = 404
+            response.status_code = 403
             return response
 
         event.rsvps.append(guest)
@@ -252,7 +210,6 @@ class RSVP(Resource):
         return response
 
 
-@api.route("/<int:event_id>/guests")
 class Guests(Resource):
     @protected_route
     def get(self, event_id):
@@ -283,47 +240,55 @@ class Guests(Resource):
         return response
 
 
-@api.route("/search")
 class Search(Resource):
     def get(self):
         return search("name")
 
 
-@api.route("/location")
 class SearchLocation(Resource):
     def get(self):
         return search("location")
 
 
-@api.route("/filter")
 class Paginate(Resource):
     # TODO add start and end links to pagination
     def get(self):
         if "limit" in request.args and request.args.get("limit"):
             limit = int(request.args.get('limit', 3))
-        else:
-            limit = 3
+
         if "offset" in request.args and request.args.get("offset"):
             offset = max(0, int(request.args.get("offset")))
-        else:
-            offset = 0
 
         ordered_events = Event.query.order_by(asc(Event.id)).all()
-        next_offset = offset + limit
-        maximum_index = len(ordered_events) - 1
-        if offset > maximum_index:
-            last_id = ordered_events[maximum_index].id
-            next_offset = offset
+        if ordered_events:
+            next_offset = offset + limit
+            maximum_index = len(ordered_events) - 1
+            if offset > maximum_index:
+                last_id = ordered_events[maximum_index].id
+                next_offset = offset
+            else:
+                last_id = ordered_events[offset].id
+
+            all_events = Event.query.filter(Event.id >= last_id).limit(limit).all()
+
+            next_url = url_for("events.paginate", limit=limit, offset=next_offset)
+            prev_url = url_for("events.paginate", limit=limit, offset=max(0, next_offset - limit))
+
+            return jsonify({
+                "events": response_helpers.parse_list("events", all_events),
+                "prev_url": prev_url,
+                "next_url": next_url
+            })
         else:
-            last_id = ordered_events[offset].id
+            return jsonify({
+                "message": "It seems there no events in the system"
+            })
 
-        all_events = Event.query.filter(Event.id >= last_id).limit(limit).all()
 
-        next_url = url_for("events.paginate", limit=limit, offset=next_offset)
-        prev_url = url_for("events.paginate", limit=limit, offset=max(0, next_offset-limit))
-
-        return jsonify({
-            "events": response_helpers.parse_list("events", all_events),
-            "prev_url": prev_url,
-            "next_url": next_url
-        })
+api.add_resource(EventList, "/")
+api.add_resource(Events, "/<int:event_id>")
+api.add_resource(RSVP, "/<int:event_id>/rsvp")
+api.add_resource(Guests, "/<int:event_id>/guests")
+api.add_resource(Search, "/search")
+api.add_resource(SearchLocation, "/location")
+api.add_resource(Paginate, "/filter")
