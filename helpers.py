ba31a0dd08a5f075fdfc6f9afe5f2d06902ccd8d
Samjunior <younggeeks101@gmail.com>
2018-01-11 01:17:15 +0300

    [Fix] move make reponse function to helpers for reusability

--- helpers.py
+++ helpers.py
@@ -1,4 +1,3 @@
-
 from flask import jsonify
 
 
@@ -27,8 +26,23 @@ def user_parser(user):
     return new_user
 
 
-# helper function used to return the message when login fails
 def failed_login():
+    """
+    helper function used to return the message when login fails
+    :return:
+    """
     response = jsonify({"message": "Wrong Combination of Username and Password"})
     response.status_code = 401
+    return response
+
+
+def make_response(status, message):
+    """
+    helper function used to return the message when request fails fails
+    :param status:
+    :param message:
+    :return:
+    """
+    response = jsonify({"message": "{}".format(message)})
+    response.status_code = status
     return response
