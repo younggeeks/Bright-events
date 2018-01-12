4a9eaf3c6c7b0e9b9ddb449629176ec43229164a
Samjunior <younggeeks101@gmail.com>
2018-01-13 00:10:13 +0300

    [Feature] add test for documentation endpoint

---
+++ tests/test_docs_page.py
@@ -0,0 +1,18 @@
+import json
+import unittest
+
+import app
+from config.config import environments
+
+
+class DocsPageTester(unittest.TestCase):
+    def setUp(self):
+        self.BASE_URL = environments["testing"].BASE_URL
+        self.app = app.app.test_client()
+        app.app.testing = True
+
+    def test_docs_route(self):
+        response = self.app.get("{}/".format(self.BASE_URL))
+        self.assertEqual(isinstance(response.data, str), True)
+        self.assertEqual(response.status_code, 200)
+
