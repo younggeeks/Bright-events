fe83c3d0c0bdc96845533f49aec33ac7f3dbc0c5
Samjunior <younggeeks101@gmail.com>
2018-01-15 02:13:24 +0300

    [Fix] include instance folder

---
+++ instance/config.py
@@ -0,0 +1,28 @@
+import os
+
+
+class Config:
+    DEBUG = False
+    SECRET = os.getenv("SECRET")
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_DATABASE_URI')
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_TEST_DATABASE_URI')
+
+
+env_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    "production": ProductionConfig
+}
