ddc4d1b25755ebc808db1380cc6aa63d3726951b
Samjunior <younggeeks101@gmail.com>
2018-01-29 22:24:06 +0300

    [Chore] update documentation for user authentication tests

--- tests/test_user_login.py
+++ tests/test_user_login.py
@@ -13,32 +13,62 @@ from tests.BaseTest import BaseTestCase
 
 class UserLoginTestCase(BaseTestCase):
     def setUp(self):
+        """
+        We make initial configurations before running the tests
+        We make sure to run setUp() from super class first
+        and then we add our file specific variable(http_helper)
+        :return:
+        """
         super(UserLoginTestCase, self).setUp()
         self.http_helpers = AuthHttpHelpers(client=self.client)
 
     def test_login_successful(self):
+        """
+        User should be able to login with email and password
+        :return:
+        """
         self.http_helpers.user_registration(correct_user)
         response = self.http_helpers.login(correct_user)
         self.assertEqual(response["status"], 200)
         self.assertEqual(response["data"]["message"], "Login successful")
 
     def test_login_unsuccessful_missing_fields(self):
+        """
+        When either of the required fields is missing
+        User should get 400 status code with relevant error message
+        :return:
+        """
         response = self.http_helpers.login(wrong_input_user)
         self.assertEqual(response["status"], 400)
         self.assertEqual(response["data"]["message"], "The following Required Field(s) are Missing: email")
 
     def test_login_unsuccessful_empty_fields(self):
+        """
+        When either of the required fields is present but the value is empty
+        User should get 400 status code with relevant error message
+        :return:
+        """
         response = self.http_helpers.login(empty_input_user)
         self.assertEqual(response["status"], 400)
         self.assertEqual(response["data"]["message"], "The following Field(s) are Empty: password, email")
 
     def test_login_unsuccessful_wrong_email(self):
+        """
+        When User enters an email that does not exist in the database
+        User should get 401 status code with relevant error message
+        :return:
+        """
         response = self.http_helpers.login(wrong_email_user)
         self.assertEqual(response["status"], 401)
         self.assertEqual(response["data"]["message"],
                          "No Account is associated with Email juma@gmail.com, Login Failed")
 
     def test_login_unsuccessful_wrong_password(self):
+        """
+        When User enters credentials that do not match with the existing ones
+        User should get 401 status code with relevant error message
+        :return:
+        """
         self.http_helpers.user_registration(correct_user)
         response = self.http_helpers.login(wrong_password_user)
         self.assertEqual(response["status"], 401)
