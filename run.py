38f348df7ccdf69fa53f64c83081f4f7a2fa3b7e
younggeeks <younggeeks101@gmail.com>
2018-04-01 16:45:18 +0300

    [Fix] add CORS to allow working with different servers names

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import os
 
 from flask import render_template
-
+from flask_cors import CORS
 from api import create_app
 
 environment = os.getenv("ENV_SETTINGS")
 app = create_app(environment)
-
+CORS(app)
 
 @app.route("/")
 def docs():
